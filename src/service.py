23a229e8201b754cc0025da73677adbfd8a5f54f
Thiago Martins <thiago.fmartins@outlook.com>
2024-11-09 17:33:04 -0300

    Add micro service interface

--- src/service.py
+++ src/service.py
@@ -1,1 +1,16 @@
 from nameko.rpc import rpc
+from dotenv import load_dotenv
+from src.agents.chat import Chat, AIMessage
+
+
+print(f'Load .env: {load_dotenv(dotenv_path="./resources/.env")}', flush=True)
+
+
+class BrunaService:
+    name = "bruna"
+
+    @rpc
+    def send(self, session_id: str, message: str) -> AIMessage:
+        chat = Chat(session_id)
+        message = chat.send(message)
+        return message
